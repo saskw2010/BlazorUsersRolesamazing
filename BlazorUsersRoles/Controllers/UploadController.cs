dfb73816769b8306224b7048f1f1d08dac87e767
mostafa elnagar <m.nagar@whitesky.tech>
2021-08-05 19:03:35 +0300

    Add project files.

---
+++ BlazorUsersRoles/Controllers/UploadController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorUsersRoles.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    // Must be in the Administrator Role
+    [Authorize(Roles = "Administrators")]
+    public class UploadController : Controller
+    {
+        private readonly IWebHostEnvironment environment;
+        public UploadController(IWebHostEnvironment environment)
+        {
+            this.environment = environment;
+        }
+
+        // Must be in the Administrator Role
+        [HttpPost("[action]")]
+        public async Task<IActionResult> MultipleAsync(
+            IFormFile[] files, string CurrentDirectory)
+        {
+            try
+            {
+                if (HttpContext.Request.Form.Files.Any())
+                {
+                    foreach (var file in HttpContext.Request.Form.Files)
+                    {
+                        // reconstruct the path to ensure everything 
+                        // goes to uploads directory
+                        string RequestedPath = 
+                            CurrentDirectory.ToLower()
+                            .Replace(environment.WebRootPath.ToLower(), "");
+
+                        if (RequestedPath.Contains("\\uploads\\"))
+                        {
+                            RequestedPath = 
+                                RequestedPath.Replace("\\uploads\\", "");
+                        }
+                        else
+                        {
+                            RequestedPath = "";
+                        }
+
+                        string path = 
+                            Path.Combine(
+                                environment.WebRootPath, 
+                                "uploads", 
+                                RequestedPath, 
+                                file.FileName);
+
+                        using (var stream = 
+                            new FileStream(path, FileMode.Create))
+                        {
+                            await file.CopyToAsync(stream);
+                        }
+                    }
+                }
+                return StatusCode(200);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ BlazorUsersRoles/Data/FileObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorUsersRoles
+{
+    public class FileObject
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+    }
+}
